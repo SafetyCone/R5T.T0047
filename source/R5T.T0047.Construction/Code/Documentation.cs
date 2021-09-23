d4e50001582d2358b2050bf567016704d09dd57d
davidcoats <david@getsafetycone.com>
2021-09-23 14:50:10 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0047.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0047.Construction
+{
+    /// <summary>
+    /// Construction project for General named file path index type (based on R5T.T0043 project name index type)..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0047/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0047
+{
+    /// <summary>
+    /// General named file path index type (based on R5T.T0043 project name index type).
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
